fe82c9f038c5e0e0f62e2e61947b19f433ea4391
Kota-35 <ishikuro6.2@gmail.com>
2026-02-10 19:18:33 +0900

    doc: p2の処理にコメントを追加

--- src/solutions/p2.rs
+++ src/solutions/p2.rs
@@ -20,35 +20,42 @@ impl Solution {
     ) -> Option<Box<ListNode>> {
         let mut sum;
         let mut carry = 0;
+        // ダミーヘッド。ノードの追加処理を簡単にするため。
         let mut l3 = Some(Box::new(ListNode::new(0)));
         let mut head = l3.as_mut();
 
         let (mut l1, mut l2) = (l1.as_ref(), l2.as_ref());
 
+        // どちらかに桁が残っている間、両方を走査する。
         while l1.is_some() || l2.is_some() {
             sum = 0;
 
             if let Some(node) = l1 {
                 sum += node.val;
+                // l1 を次へ進める。
                 l1 = node.next.as_ref()
             }
 
             if let Some(node) = l2 {
                 sum += node.val;
+                // l2 を次へ進める。
                 l2 = node.next.as_ref()
             }
 
             sum += carry;
             carry = sum / 10;
             sum %= 10;
+            // 現在の桁を追加する。
             head.as_mut().unwrap().next = Some(Box::new(ListNode::new(sum)));
             head = head.unwrap().next.as_mut()
         }
 
+        // 繰り上がりが残っていれば追加する。
         if carry != 0 {
             head.as_mut().unwrap().next = Some(Box::new(ListNode::new(carry)))
         }
 
+        // ダミーヘッドを捨てて実際の先頭を返す。
         l3.unwrap().next
     }
 }
